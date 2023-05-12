8a48a0f167f5f079d5f75f025ca12b72c5514957
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2023-05-12 23:09:42 +0100

    Styling mobile css

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
 import CartIcon from './assets/images/icon-cart.svg'
+import MobileImage from './assets/images/image-product-mobile.jpg'
+import DesktopImage from './assets/images/image-product-desktop.jpg'
 
 function App() {
   return (
-    <div className='bg-cream h-screen'>
-      <div id='card'>
-        <div id='image'>
-          <img
-            src='./assets/images/image-product-mobile.jpg'
-            alt='Mobile Image'
-          />
-        </div>
+    <div className='bg-cream h-screen flex items-center justify-center'>
+      <div id='card' className='w-[375px] bg-white rounded-lg'>
+        <img src={MobileImage} alt='Mobile Image' className='rounded-t-lg' />
 
-        <div id='content'>
+        <div
+          id='content'
+          className='font-montserrat font-medium m-4 text-very-dark-blue'
+        >
           <div id='header'>
-            <h2>Perfume</h2>
-            <h1>Gabrielle Essence Eau De Parfum</h1>
+            <h2 className='uppercase tracking-[7px] text-dark-grayish-blue text-sm mb-2'>
+              Perfume
+            </h2>
+            <h1 className='font-fraunces text-3xl'>
+              Gabrielle Essence Eau De Parfum
+            </h1>
           </div>
 
-          <p>
+          <p className='text-dark-grayish-blue text-sm my-4'>
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
 
-          <div id='price'>
-            <h1>$149.99</h1>
-            <h3 id='before-discount'>$169.99</h3>
+          <div id='price' className='my-4 flex justify-start items-center'>
+            <h1 className='font-fraunces text-3xl text-dark-cyan mr-4'>
+              $149.99
+            </h1>
+            <h3
+              id='before-discount'
+              className='text-dark-grayish-blue line-through text-sm'
+            >
+              $169.99
+            </h3>
           </div>
 
-          <button type='button'>
-            <img src={CartIcon} alt='Cart Icon' />
+          <button
+            type='button'
+            className='w-full rounded-lg bg-dark-cyan hover:bg-very-dark-blue flex items-center justify-center text-white py-3'
+          >
+            <img src={CartIcon} alt='Cart Icon' className='mr-3' />
             Add to Cart
           </button>
         </div>
